ddd119883bc7bf40fed9f1ec17f5c7b3b5e931c1
alexschepers <ans91118@uga.edu>
2022-03-30 13:44:15 -0400

    Added timestamp to quiz POJO

--- app/src/main/java/edu/uga/cs/project4/Quiz.java
+++ app/src/main/java/edu/uga/cs/project4/Quiz.java
@@ -1,10 +1,15 @@
 package edu.uga.cs.project4;
 
+import java.security.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Quiz {
 
     private long id;
     private Question[] questionArray;
     private long score;
+    private String time;
 
     public Quiz() {
         this.id = -1;  // the primary key id will be set by a setter method
@@ -18,6 +23,7 @@ public class Quiz {
 
         // set score like this until it can be calculated somehow
         this.score = -1;
+        setTime();
     }
 
     public long getId() {
@@ -40,6 +46,12 @@ public class Quiz {
     // not sure how to calculate score at this point
     public void setScore(long score) {
         this.score = score;
+    }
+
+    public void setTime() {
+        SimpleDateFormat date = new SimpleDateFormat("yyyy.MM.dd.HH:mm:ss");
+        String timeStamp = date.format(new Date());
+        this.time = timeStamp;
     }
 
 }
