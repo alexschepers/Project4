6491f5c9e0bda3a5d020ac6e4c367f2df7f55b4a
maggie <mmo34985@uga.edu>
2022-04-06 13:36:56 -0400

    Started Fragment for actual quiz. Need to figure out how to swipe to next question and update question

--- app/src/main/java/edu/uga/cs/project4/StartQuizActivity.java
+++ app/src/main/java/edu/uga/cs/project4/StartQuizActivity.java
@@ -3,12 +3,41 @@ package edu.uga.cs.project4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class StartQuizActivity extends AppCompatActivity {
+    //private Question
+    private TextView mScoreView;
+    private TextView mQuestionView;
+    private RadioButton mButtonChoice1;
+    private RadioButton mButtonChoice2;
+    private RadioButton mButtonChoice3;
+
+    private String mAnswer;
+    private int mScore = 0;
+    private int mQuestionNumber = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_quiz);
+
+        //mScoreView = (TextView)findViewById(R.id.score);
+        mQuestionView = (TextView)findViewById(R.id.quizQuestion);
+        mButtonChoice1 = (RadioButton)findViewById(R.id.option1);
+        mButtonChoice2 = (RadioButton)findViewById(R.id.option2);
+        mButtonChoice3 = (RadioButton)findViewById(R.id.option3);
+
+        mButtonChoice2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(mButtonChoice1.getText() == mAnswer){
+                    mScore += 1;
+                }
+            }
+        });
     }
+
 }
